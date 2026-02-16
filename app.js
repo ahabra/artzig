be0c7e8c069a4361f8b2ed447d7a3e43972411bf
Abdul Habra <abdul.habra@gmail.com>
2026-02-16 00:17:36 -0500

    restore selected iframe when refreshing page

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 (function() {
-globalThis.app ={
+const public = {
   toggleSidenav
 }
+
+globalThis.app = {...globalThis.app, ...public}
 
 const RESPONSIVE_WIDTH = 600;
 const sidenav = document.querySelector("#sidenav");
@@ -19,5 +21,30 @@ function toggleSidenav() {
 function isMobile() {
   return window.matchMedia(`only screen and (max-width: ${RESPONSIVE_WIDTH}px)`).matches
 }
+
+
+const iframe = document.getElementById('contentFrame')
+
+function showLinksInIframe() {
+  const links = document.querySelectorAll('a.in-frame')
+  links.forEach(link => {
+    link.onclick = ev => {
+      ev.preventDefault()
+      iframe.src = link.href
+      app.session.setIframe(link.href)
+    }
+  })
+
+}
+
+function restoreIframe() {
+  const src = app.session.getIframe()
+  if (src) {
+    iframe.src = src
+  }
+}
+
+showLinksInIframe()
+restoreIframe()
 
 })();
---
+++ js/session.js
@@ -0,0 +1,20 @@
+(function() {
+
+const public = {
+  session: {
+    getIframe, setIframe
+  }
+}
+globalThis.app = {...globalThis.app, ...public}
+
+function getIframe() {
+  return sessionStorage.getItem('iframeSrc')
+}
+
+function setIframe(iframeSrc) {
+  sessionStorage.setItem('iframeSrc', iframeSrc)
+}
+
+
+
+})();
