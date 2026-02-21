76dca11893397f7b13b07dd8f5bceedb36ddd0df
Abdul Habra <abdul.habra@gmail.com>
2026-02-20 22:52:29 -0500

    remove unused code, was attempting to size modal based on image

--- js/modal.js
+++ js/modal.js
@@ -36,37 +36,11 @@ function clearModal() {
   }
 }
 
-function adjustSize() {
-  const modalContent = document.querySelector('.modal-content')
-  const img = modalContent.querySelector('img')
-
-  img.addEventListener('load', ()=> {
-    const imgHeight = img.height
-    const imgWidth = img.width
-
-    console.log('img Width=', imgWidth, 'Height=', imgHeight)
-    console.log('modalContent Width=', modalContent.offsetWidth, 'Height=', modalContent.offsetHeight)
-
-    // if (modalContent.height > imgHeight) {
-      modalContent.style.height= `${400}px`
-    // }
-
-    // if (modalContent.width > imgWidth) {
-      modalContent.style.width= `${1000}px`
-    // }
-  })
-
-  if (img.complete) {
-    img.dispatchEvent(new Event('load'))
-  }
-
-}
 
 function showModal(imgSrc, alt='') {
   clearModal()
   const html = buildHtml(imgSrc, alt)
   document.body.insertAdjacentHTML('beforeend', html)
-  // adjustSize()
   defineClosers()
   getModal().style.display = 'block'
 }
