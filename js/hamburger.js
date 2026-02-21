37703f86de64c444516a489bcd93a6943b085164
Abdul Habra <abdul.habra@gmail.com>
2026-02-21 12:08:03 -0500

    simplify hamburger's css

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,10 @@
-import * as utils from './utils.js'
-
 const sidenav = document.querySelector("#sidenav");
 const ham = document.querySelector('.hamburger')
 
 export function defineClickHandler() {
   ham.onclick = () => {
     ham.classList.toggle('rotate90Deg')
-    if (utils.isMobile()) {
-      sidenav.classList.remove('min');
-      sidenav.classList.toggle('active')
-    } else {
-      sidenav.classList.remove('active')
-      sidenav.classList.toggle('min')
-    }
-
+    sidenav.classList.toggle('active')
   }
 }
 
