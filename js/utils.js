45e3a9c2296a4f6618527777107157d6f5fe947f
Abdul Habra <abdul.habra@gmail.com>
2026-03-09 23:04:00 -0400

    working on welcome page

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,33 @@ const RESPONSIVE_WIDTH = 600;
 export function isMobile() {
   return window.matchMedia(`only screen and (max-width: ${RESPONSIVE_WIDTH}px)`).matches
 }
+
+// art.now.247@gmail.com
+
+// art_$$_n[the fox]ow_$$_247=ser[a rt]ver_$$_com[42 pages]
+
+/**
+ * decode an obfuscated email, to help with spammers
+ * 1. remove all square brackets and their content
+ * 2. replace equals sign with at sign
+ * 3. replace "_$$_" with dot
+ * 4. replace "server" with "gmail"
+ * For Example:
+ * foo_$$_n[the fox]ow_$$_bar=ser[a rt]ver_$$_com[42 pages]
+ * will become foo.now.bar@gmail.com
+ */
+function decodeEmail(encoded) {
+  const regex = /\[.*?\]/g
+  return encoded.replaceAll(regex, '')
+    .replace('=', '@')
+    .replaceAll('_$$_', '.')
+    .replace('server', 'gmail')
+}
+
+
+export function displayEmail() {
+  document.querySelectorAll('.email').forEach(el => {
+    const email = el.innerText.trim()
+    el.innerText = decodeEmail(email)
+  })
+}
